ad5d8c9fbfc9aef77593f470cf339cfc101728e7
Programming hero <rahman22205101894@diu.edu.bd>
2025-08-06 20:28:46 +0600

    find Application add

--- index.js
+++ index.js
@@ -6,40 +6,58 @@ const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
-app.use(express.json()); //scholarship
-                         // XBQora7dBcTMsBRZ
+app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER }:${process.env.DB_PASS }@cluster0.cjuyyb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjuyyb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        //Find scholarship related apis
+
+        const findScholarship = client.db('scholarshipBD').collection('findScholarship');
+        const apply = client.db('scholarshipBD').collection('apply');
+
+        app.get('/findScholarship', async (req, res) => {
+            const scholarship = await findScholarship.find().toArray();
+            res.send(scholarship);
+        });
+
+        app.post('/apply', async(req, res) =>{
+            const formData = req.body;
+            const result = await apply.insertOne(formData);
+            res.send({success: true, insertedId: result.insertedId});
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
+
+
+app.get('/', (req, res) => {
     res.send('Scholarship is falling in the Scholarship BD');
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`scholarship is waiting for you: ${port}`)
 });
