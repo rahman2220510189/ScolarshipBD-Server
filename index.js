6135e5c0e1c0fcc94dd034c74c97c7f0826b8c60
unknown <rahman22205101894@diu.edu.bd>
2025-10-26 22:11:55 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -87,8 +87,8 @@ async function run() {
         });
 
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
 run().catch(console.dir);
